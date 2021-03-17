49c870877c9faf143480b7eb3d534a1ac50bb521
khuedo <dominhkhue@gmail.com>
2021-03-16 20:21:49 -0700

    resut -> result

--- app/src/main/java/com/example/weatherapp2/MainActivity.java
+++ app/src/main/java/com/example/weatherapp2/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.weatherapp2;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 //import android.support.v4.app.ActivityCompat;
 //import android.support.v4.content.ContextCompat;
 //import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -68,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
     public void search(View view){
         cityName = findViewById(R.id.cityName);
         searchButton = findViewById(R.id.searchButton);
-        result = findViewById(R.id.resut);
+        result = findViewById(R.id.result);
 
         String cName = cityName.getText().toString();
 
